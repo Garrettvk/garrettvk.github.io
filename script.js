d5b6bdf596b67743c55b7e0606a326bec79831ab
Garrett <gvk795@gmail.com>
2020-04-25 10:50:18 -0700

    webpage is working great

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function get_play_rate () {
   // define variables for html elements
-  song_bpm_input = document.getElementById('song_bpm_id')
-  sample_bpm_input = document.getElementById('sample_bpm_id')
+  song_bpm_input = document.getElementById('song_bpm_id').value
+  sample_bpm_input = document.getElementById('sample_bpm_id').value
   results_element = document.getElementById('results')
 
+  // this clears the text every time button is pressed
+  results_element.innerHTML = ''
+
+  results = `Change your sample's playrate to ${song_bpm_input /
+    sample_bpm_input}`
+
   // print results to webpage
-  results_element.innerHTML += song_bpm_input.value;
+  results_element.innerHTML += results
 }
