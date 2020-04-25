f07bc1c36d01ba75401fbbe8b0e9660ef3c424ba
Garrett <gvk795@gmail.com>
2020-04-25 10:36:28 -0700

    input printed to webpage

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-song_bpm_input = document.getElementById("song_bpm_id");
-sample_bpm_input = document.getElementById("sample_bpm_id");
-results_element = document.getElementById("results");
+function get_play_rate () {
+  // define variables for html elements
+  song_bpm_input = document.getElementById('song_bpm_id')
+  sample_bpm_input = document.getElementById('sample_bpm_id')
+  results_element = document.getElementById('results')
+
+  // print results to webpage
+  results_element.innerHTML += song_bpm_input.value;
+}
